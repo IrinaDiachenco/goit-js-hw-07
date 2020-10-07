69dbb9724165c88b8fd6fec21f04856409a817af
Irina Diachenco <bilairina@ukr.net>
2020-10-07 21:52:48 +0300

    5 и 6 задание

--- task-4.js
+++ task-4.js
@@ -1,1 +1,13 @@
-const decrementBtn = document.querySelector('[data-action="decrement"]');
+let counterValue = 0;
+counterValue = ++document.querySelector("#value").textContent;
+console.log(`Значение счетчика: ${counterValue}`);
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const handleDecrementBtnClick = () => console.log((Number(counterValue) += 1));
+console.log(`Значение счетчика: ${counterValue}`);
+decrementBtn.addEventListener("click", handleDecrementBtnClick);
+
+const incrementBtn = document.querySelector('button[data-action="decrement"]')
+  .textContent;
+
+console.log(`Значение счетчика: ${counterValue}`);
+incrementBtn.addEventListener("click", handleDecrementBtnClick);
