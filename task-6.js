f98ffdbfd86f4bde908324bc541aa4c099653a99
Irina Diachenco <bilairina@ukr.net>
2020-10-11 09:22:15 +0300

    Update task-6.js

--- task-6.js
+++ task-6.js
@@ -1,12 +1,12 @@
 const validationInput = document.querySelector("#validation-input");
 
 function inputFocusChange() {
-  if (validationInput.value.length < validationInput.dataset.length) {
-    validationInput.classList.remove("invalid");
-    validationInput.classList.add("valid");
-  } else {
+  if (validationInput.value.length != validationInput.dataset.length) {
     validationInput.classList.remove("valid");
     validationInput.classList.add("invalid");
+  } else {
+    validationInput.classList.remove("invalid");
+    validationInput.classList.add("valid");
   }
 }
 validationInput.addEventListener("blur", inputFocusChange);
